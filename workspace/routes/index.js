e22e08b227556a0e01ad302c58cd78aa84db4153
hhjung1202 <hhjung1202@naver.com>
2018-11-09 17:00:21 +0900

    path check

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../model');
-var service = require('../service/service')
+var service = require('../service/file_service')
 
 /* GET home page. */
 router.get('/', function(req, res) {
