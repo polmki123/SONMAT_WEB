34234424e0296742bbd35a15f82245576739f654
ksj01401 <ksj01401@gmail.com>
2018-11-12 05:16:16 +0900

    업로드 버튼 기능 구현 완료

--- workspace/routes/handwrite.js
+++ workspace/routes/handwrite.js
@@ -2,16 +2,15 @@ var express = require('express');
 var router = express.Router();
 var models = require('../model');
 
-var file_service = require('../service/file_service');
-
 router.get('/upload', function (req, res) {
     res.render('handwrite-upload');
 });
 
 router.post('/file', function(req, res) {
 
-    //console.log(req.body);
-    //file_service.image_dir_change(req.files.file, 'font', '1', '1');
+    res.send(req.body);
+
+    //
 });
 
 router.get('/making/start', function(req, res) {
