1ecc775cfe867aca4913f68b2ca41f0da8cc203d
headcha <headcha@naver.com>
2018-11-12 23:11:53 +0900

    layout 작업 완료

--- workspace/routes/mento/view/message/message.js
+++ workspace/routes/mento/view/message/message.js
@@ -10,5 +10,13 @@ router.get('/form', function(req, res, next) {
     res.render('message/form');
 });
 
+router.get('/to/:toUserId/timeline', function(req, res, next) {
+    res.render('message/timeline');
+});
+
+router.get('/to/:toUserId/:messageId', function(req, res, next) {
+    res.render('message/detail');
+});
+
 
 module.exports = router;
