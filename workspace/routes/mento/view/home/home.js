be1e4b03559211e7363c193405adff97907c3dad
headcha <headcha@naver.com>
2018-11-12 23:22:57 +0900

    actuator 기능 추가

--- workspace/routes/mento/view/home/home.js
+++ workspace/routes/mento/view/home/home.js
@@ -6,5 +6,15 @@ router.get('/', function(req, res, next) {
     res.render('home/home');
 });
 
+router.get('/shutdown', function(req, res, next) {
+    process.exit(0);
+    //
+})
+
+router.get('/ping', function(req, res, next) {
+    res.sendStatus(200);
+})
+
+
 
 module.exports = router;
