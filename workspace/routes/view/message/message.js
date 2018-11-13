db642b841f128997e24c6dbf0a39e1d8a9d63f46
unknown <hhjung1202@naver.com>
2018-11-14 01:37:38 +0900

    timeline render

--- workspace/routes/view/message/message.js
+++ workspace/routes/view/message/message.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var msgB_service = require('../../../service/message_box_service')
 
 /* index */
 router.get('/', function(req, res, next) {
-    res.render('message/list');
+	res.render('message/list');
 });
 
 router.get('/form', function(req, res, next) {
-    res.render('message/form');
+	res.render('message/form');
 });
 
 router.get('/to/:toUserId/timeline', function(req, res, next) {
-    res.render('message/timeline');
+	var render_data = { uid: req.user.id, }
+	var opponent_uid = req.params.toUserId
+	msgB_service.get_opponents_name(req.user.id) // user
+	.then(function(opponents){
+		render_data.opponents = opponents;
+		return msgB_service.get_message_timeline(req.user.id, opponent_uid)
+	}).then(function(msgs){
+		render_data.msgs = msgs;
+		res.render('message/timeline', render_data);
+	}).catch(function(err) {
+		console.log(err);
+		next()
+	});
+
 });
 
 router.get('/to/:toUserId/:messageId', function(req, res, next) {
-    res.render('message/detail');
+	var opponent_uid = req.params.toUserId
+	var msg_id = req.params.messageId
+	res.render('message/detail');
 });
 
 
