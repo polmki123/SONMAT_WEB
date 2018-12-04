3855c863a6621d584cce9ff36ccd3a9470d927d1
ksj01401 <ksj01401@gmail.com>
2018-12-04 17:53:36 +0900

    폰트 url 저장하는 함수명 변경

--- workspace/service/font_service.js
+++ workspace/service/font_service.js
@@ -229,7 +229,7 @@ function formatDate(date_time){
     return yyyy+'/'+MM+'/'+dd+' '+hh+':'+mm;
 }
 
-function match_font_files(font_id, font_urls) {
+function save_font_urls(font_id, font_urls) {
 
     var promises = [];
 
@@ -269,5 +269,6 @@ func.notify_complete = notify_complete;
 func.get_font_list = get_font_list;
 func.my_font_gallery_font_id = my_font_gallery_font_id;
 func.my_font_gallery_user_id = my_font_gallery_user_id;
+func.save_font_urls = save_font_urls;
 
 module.exports = func;
