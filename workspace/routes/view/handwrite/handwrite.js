1200794c7ce7bb03f2e70e36f0306549dd511352
ssunhye <wkadnsj@gmail.com>
2018-12-06 12:06:29 +0900

    손글씨 제작 가이드 화면

--- workspace/routes/view/handwrite/handwrite.js
+++ workspace/routes/view/handwrite/handwrite.js
@@ -5,6 +5,11 @@ router.get('/', function (req, res) {
     res.render('handwrite/writing', res.render_data);
 });
 
+router.get('/taking_picture', function(req, res) {
+
+    res.render('handwrite/taking_picture', res.render_data);
+});
+
 router.get('/form', function (req, res) {
 
     res.render('handwrite/form' , res.render_data);
